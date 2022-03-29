b5120924cfed5af007bbae314ccd5efa56171315
myxomatosis <myxo@magik.tk>
2022-03-28 22:17:33 -0400

    Add logging and other improvements

--- collector.py
+++ collector.py
@@ -2,7 +2,21 @@
 import http.client
 import json
 from os.path import exists, getsize
+import logging
 
+# set up logging to file
+logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s %(levelname)-8s %(message)s',
+                                                                    filename='/var/log/tpdne.log')
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(levelname)-8s %(message)s')
+# tell the handler to use this format
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
 
 def get_link():
     conn = http.client.HTTPSConnection("this-person-does-not-exist.com")
@@ -14,41 +28,50 @@ def get_link():
     return data, status
 
 
-def get_image():
-    response = get_link()
-    file_name = "ai_generated_pictures/" + response[0]["name"][7:]
-    if response[1] == 200:
-        if not exists(file_name):
-            conn = http.client.HTTPSConnection("this-person-does-not-exist.com")
-            conn.request("GET",f"/img/{response[0]['name']}")
-            res = conn.getresponse()
-            data = res.read()
-            if res.status == 200:
-                with open(file_name,'xb') as f:
-                    f.write(data)
-                print('Image sucessfully Downloaded: ', file_name)
-            else:
-                print(f"Invalid response from get_image(). Response code: {res.status}")
-        else:
-            if getsize(file_name) == 0:
+def get_image(num=1000000):
+    for val in range(1, num + 1):
+        response = get_link()
+        file_name = "ai_generated_pictures/" + response[0]["name"][7:]
+        if response[1] == 200:
+            if not exists(file_name):
                 conn = http.client.HTTPSConnection("this-person-does-not-exist.com")
                 conn.request("GET",f"/img/{response[0]['name']}")
                 res = conn.getresponse()
                 data = res.read()
-                conn.close()
                 if res.status == 200:
-                    with open(file_name,'wb') as f:
+                    with open(file_name,'xb') as f:
                         f.write(data)
-                    print('Image sucessfully Downloaded: ', file_name)
+                    logging.info(f'{val}\t==>Image sucessfully Downloaded: {file_name}')
                 else:
-                    print(f"File exists: {file_name}")
-    else:
-        print(f"Invalid response from get_link(). Response code: {response[1]}")
+                    logging.warn(f"Invalid response from get_image(). Response code: {res.status}")
+            else:
+                if getsize(file_name) == 0:
+                    conn = http.client.HTTPSConnection("this-person-does-not-exist.com")
+                    conn.request("GET",f"/img/{response[0]['name']}")
+                    res = conn.getresponse()
+                    data = res.read()
+                    if res.status == 200:
+                        with open(file_name,'wb') as f:
+                            f.write(data)
+                        logging.info(f'{val}\t==>Image sucessfully Downloaded: {file_name}')
+                    else:
+                        logging.warn(f"File exists: {file_name}")
+        else:
+            logging.warn(f"Invalid response from get_link(). Response code: {response[1]}")
+    conn.close()
 
 
 def main():
-    get_image()
-
+    logging.info(f"Starting New Session")
+    while True:
+        logging.info(f"How many images do you want?")
+        val = input("How many images do you want? ")
+        try:
+            logging.info(f"You have chosen {val}")
+            get_image(int(val))
+            break
+        except:
+                logging.warn(f"Invalid input provided. Please choose a number.")
 
 if __name__=="__main__":
     main()
